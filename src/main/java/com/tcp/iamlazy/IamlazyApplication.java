958759b5510a9c66cb0b6849a2f1766b42665e03
jae-ran <coxo9535@gmail.com>
2020-03-08 02:34:39 +0900

    Update Server Setting

---
+++ src/main/java/com/tcp/iamlazy/IamlazyApplication.java
@@ -0,0 +1,13 @@
+package com.tcp.iamlazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamlazyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamlazyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tcp/iamlazy/IamlazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcp.iamlazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamlazyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
