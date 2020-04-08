46fbd70657f95eb850725f3ff547947ec22d84e5
foxrain <kimilb412@gmail.com>
2020-04-09 02:15:22 +0900

    Put test method add.

--- src/test/java/com/tcp/iamlazy/todo/controller/TodoControllerTest.java
+++ src/test/java/com/tcp/iamlazy/todo/controller/TodoControllerTest.java
@@ -2,6 +2,8 @@ package com.tcp.iamlazy.todo.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.tcp.iamlazy.configuration.security.CustomUserDetailsService;
+import com.tcp.iamlazy.configuration.security.UserPrincipal;
 import com.tcp.iamlazy.todo.contoller.ToDoController;
 import com.tcp.iamlazy.todo.service.ToDoService;
 import org.junit.Before;
@@ -9,10 +11,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
  * Created with intellij IDEA. by 2020 04 2020/04/09 12:41 오전 09 User we at 00 41 To change this
@@ -28,7 +38,9 @@ public class TodoControllerTest {
 
   @Before
   public void setUp() {
-    this.mockMvc = MockMvcBuilders.standaloneSetup(new ToDoController(toDoService)).build();
+    this.mockMvc = MockMvcBuilders.standaloneSetup(new ToDoController(toDoService))
+        .setCustomArgumentResolvers(putAuthenticationPrincipal)
+        .build();
     /* maybe you can use webMvcTest instead */
   }
 
@@ -126,4 +138,42 @@ public class TodoControllerTest {
         .andExpect(status().isCreated());
 
   }
+
+  @Test
+//  @WithMockUser(username = "admin", authorities = { "ADMIN", "USER" })
+//  @WithUserDetails(value = "1313651068", userDetailsServiceBeanName = "customUserDetailsService")
+  public void Todo_데이터_업데이트_타당성_검사_WhenAllFieldAreGoodShouldReturnSuccess()
+      throws Exception {
+
+    this.mockMvc.perform(MockMvcRequestBuilders
+                             .put("/todos/{id}", "3")
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .content("{\n"
+                                          + "\t\"todoTitle\" : \"title\",\n"
+                                          + "\t\"todoContent\" : \"content\",\n"
+                                          + "\t\"todoDate\" : \"2020-03-29\",\n"
+                                          + "\t\"repeatableYN\" : 1,\n"
+                                          + "\t\"repeatUnit\" : 1,\n"
+                                          + "\t\"startDate\" : \"2020-03-29\",\n"
+                                          + "\t\"endDate\" : \"2020-04-30\",\n"
+                                          + "\t\"weekDay\" : 2,\n"
+                                          + "\t\"monthDay\" : 3,\n"
+                                          + "\t\"finish\" : 0\n"
+                                          + "}"))
+        .andExpect(status().isOk());
+
+  }
+
+  private HandlerMethodArgumentResolver putAuthenticationPrincipal = new HandlerMethodArgumentResolver() {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+      return parameter.getParameterType().isAssignableFrom(UserPrincipal.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+      return new UserPrincipal(3L, "email", "pass", AuthorityUtils.createAuthorityList("USER"));
+    }
+  };
 }
